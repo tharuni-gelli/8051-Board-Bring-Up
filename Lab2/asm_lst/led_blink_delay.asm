c89cb06abfc23e4732bb0ad5a29f182ac7113e58
Tharuni gelli <143668242+tharuni-gelli@users.noreply.github.com>
2024-04-05 00:13:27 -0600

    Add files via upload

---
+++ Lab2/asm_lst/led_blink_delay.asm
@@ -0,0 +1,35 @@
+ORG 0000H //  start of program
+      LJMP MAIN_CODE // long jump to main program
+
+ORG 000BH	// start of interrupt service routine
+	  LJMP ISR_TIMER //long jump to ISR code
+	
+MAIN_CODE: //start of main code
+          MOV TMOD,#00000001B // initializing TMOD to timer 0 and 16bit
+	      MOV A,#0x00 // accumalator as 0
+          MOV P1,A // making port 1 as output port
+LOOP:     CLR TR0 // clear timer run bit
+          MOV R3,#0X06 // load register 3 with R5
+COUNTER:  MOV TH0,#0x00 // Load higher bit of timer 0 with 00 (for max count of FFH)
+	      MOV TL0,#0x00 // Load lower bit of timer 0 with 00 (for max count of FFH)
+	      SETB TR0		  // start the counter
+BACK1:    JNB TF0, BACK1 // Loop here untill the count is completed
+          CLR TR0 // clear the bit timer run
+          CLR TF0  // Clear interupt overflow
+		  DJNZ R3, COUNTER // decrement the register value and move to counter r3
+	      MOV IE,#0x82 // enable the interupt enable register for timer 0
+	      MOV TH0,#0xE8 // load timer 0 for remaining counts
+	      MOV TL0,#0xB9 // load timer 0 for remaining counts
+	      SETB TR0 // set timer run 0 bit
+BACK2:    JNB TF0, BACK2 // loop here untill its completed (count value 5963)
+ 
+
+
+ISR_TIMER: SETB P1.0 // set pin (toggle unused pin)
+           CPL P1.1 // toggle led
+		   CLR P1.0 // clear pin (toggle unused pin)
+		   SJMP LOOP // go to main loop infinite times
+		   
+		   RETI // return to next instruction of interupt end
+ 
+END // end of program
